3b8da4ac269ed111ee8320a490446630f0523d5e
Pink Moonlight <kate.cherie25@gmail.com>
2020-04-29 17:14:54 -0700

    added functionality to the phone menu and added to about page too

--- js/main.js
+++ js/main.js
@@ -6,23 +6,24 @@
 // 2. Write handle event function
 // 3. Set up event listeners
 
-let menu = document.querySelector("navigation__icon");
-let closeButton = document.querySelector("phone-nav__close_icon");
-let phoneNav = document.querySelector("phone-nav");
+let menu = document.querySelector(".navigation__icon");
+let closeButton = document.querySelector(".phone-nav__close_icon");
+let phoneNav = document.querySelector(".phone-nav");
+let menuLinks = document.querySelectorAll(".phone-nav__item");
 
 let menuToggle = (e) => {
     //let menuOpen = false;
-
     console.log(e.target);
-    if(e.target === menu) {
-        phoneNav.toggleAttribute(".menu-open");
-        menu.toggleAttribute(".invisible");
-    } else if(e.target === closeButton) {
-        phoneNav.toggleAttribute(".menu-open");
-        menu.toggleAttribute(".invisible");
-    }
+
+    phoneNav.classList.toggle("menu-open");
+    menu.classList.toggle("navigation__invisible");
+    //phoneNav.toggleAttribute(".menu-open");
+    //menu.toggleAttribute(".invisible");
 };
 
-menu.addEventListener("onClick", menuToggle);
-closeButton.addEventListener("onClick", menuToggle);
+menu.addEventListener("click", menuToggle);
+//menu.addEventListener("touchend", menuToggle);
+closeButton.addEventListener("click", menuToggle);
+//closeButton.addEventListener("touchend", menuToggle);
+menuLinks.forEach(link => link.addEventListener('click', menuToggle));
 
